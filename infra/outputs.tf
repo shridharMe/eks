0855dad4d39736fbce02af1bd3a34d8d484d0b99
shridharMe <shridhar_m_p@yahoo.co.in>
2025-06-10 11:49:20 +0000

    intial commit

---
+++ infra/outputs.tf
@@ -0,0 +1,4 @@
+
+# output "karpenter_instance_profile" {
+#   value = module.eks_blueprints_addons.karpenter.node_instance_profile_name
+# }
---
+++ infra/vpc.tf
@@ -0,0 +1,52 @@
+
+data "aws_availability_zones" "available" {
+  filter {
+    name   = "opt-in-status"
+    values = ["opt-in-not-required"]
+  }
+}
+
+locals {
+  name            = "subnet-k8s-testing"  
+  azs=slice(data.aws_availability_zones.available.names, 0, 3)
+  vpc_cidr="10.0.0.0/16"
+  public_subnets  = ["10.0.3.0/24","10.0.4.0/24","10.0.5.0/24"]
+  private_subnets = ["10.0.0.0/24", "10.0.1.0/24", "10.0.2.0/24"]
+  tags={}
+  cluster_version ="1.30"
+}
+
+module "vpc" {
+  source  = "terraform-aws-modules/vpc/aws"
+  version = "5.12.1"
+
+  name = local.name
+  cidr = local.vpc_cidr
+
+  azs             =  local.azs
+  public_subnets  = local.public_subnets
+  private_subnets = local.private_subnets
+
+  enable_nat_gateway   = true
+  single_nat_gateway   = true
+  enable_dns_hostnames = true
+
+  # Manage so we can name
+  manage_default_network_acl    = true
+  default_network_acl_tags      = { Name = "${local.name}-default" }
+  manage_default_route_table    = true
+  default_route_table_tags      = { Name = "${local.name}-default" }
+  manage_default_security_group = true
+  default_security_group_tags   = { Name = "${local.name}-default" }
+
+  public_subnet_tags = {
+    "kubernetes.io/cluster/${local.name}" = "shared"
+    "kubernetes.io/role/elb"              = 1
+  }
+
+  private_subnet_tags = {
+    "kubernetes.io/cluster/${local.name}" = "shared"
+    "kubernetes.io/role/internal-elb"     = 1
+    "karpenter.sh/discovery"              = local.name
+  }
+}
